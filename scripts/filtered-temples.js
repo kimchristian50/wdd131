2966f4fe8c3346e9691763805bd487e60c99218f
kimchristian50 <pchristison@byupathway.edu>
2026-03-25 14:38:58 -0600

    saved pictures locally, did more optimization

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -30,145 +30,148 @@ const temples = [
         location: "Aba, Nigeria",
         dedicated: "2005, August, 7",
         area: 11500,
-        imageUrl: "images/aba-nigeria-temple.jpg";
-        // "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        imageUrl: "images/aba-nigeria-temple.jpg"
     },
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, United States",
         dedicated: "1888, May, 21",
         area: 74792,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        imageUrl: "images/manti-temple.jpg"
     },
     {
         templeName: "Payson Utah",
         location: "Payson, Utah, United States",
         dedicated: "2015, June, 7",
         area: 96630,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        imageUrl: "images/payson-utah-temple.jpg"
     },
     {
         templeName: "Yigo Guam",
         location: "Yigo, Guam",
         dedicated: "2020, May, 2",
         area: 6861,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        imageUrl: "images/yigo-guam-temple.webp"
     },
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, United States",
         dedicated: "1974, November, 19",
         area: 156558,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        imageUrl: "images/washington-dc-temple.jpeg"
     },
     {
         templeName: "Lima Perú",
         location: "Lima, Perú",
         dedicated: "1986, January, 10",
         area: 9600,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        imageUrl: "images/lima-peru-temple.jpg"
     },
     {
         templeName: "Mexico City Mexico",
         location: "Mexico City, Mexico",
         dedicated: "1983, December, 2",
         area: 116642,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        imageUrl: "images/mexico-city-temple.jpg"
     },
-    // {
-    // templeName: "Paris France",
-    // location: "Paris, France",
-    // dedicated: "2017, May, 21",
-    // area: 44175,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg"
-    // },
+    {
+        templeName: "Paris France",
+        location: "Paris, France",
+        dedicated: "2017, May, 21",
+        area: 44175,
+        imageUrl: "images/paris-france-temple.jpg"
+    },
     {
         templeName: "Rome Italy",
         location: "Rome, Italy",
-        dedicated: "2019, March, 10-12",
+        dedicated: "2019, March, 10",
         area: 41010,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg"
+        imageUrl: "images/rome-temple.jpg"
     },
-    // {
-    // templeName: "Bern Switzerland",
-    // location: "Bern, Switzerland",
-    // dedicated: "1955, September, 11-15",
-    // area: 35546,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/bern-switzerland-temple/bern-switzerland-temple-54641-main.jpg"
-    // },
-    // {
-    // templeName: "Copenhagen Denmark",
-    // location: "Copenhagen, Denmark",
-    // dedicated: "2004, May, 23",
-    // area: 25000,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/copenhagen-denmark-temple/copenhagen-denmark-temple-16169-main.jpg"
-    // },
-    // {
-    // templeName: "Lubbock Texas",
-    // location: "Lubbock, Texas",
-    // dedicated: "2002, April, 21",
-    // area: 16498,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/lubbock-texas-temple/lubbock-texas-temple-13996-main.jpg"
-    // },
-    // {
-    // templeName: "Jordan River Utah",
-    // location: "South Jordan, Utah",
-    // dedicated: "1981, November, 16-20",
-    // area: 148236,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/jordan-river-utah-temple/jordan-river-utah-temple-3449.jpg"
-    // },
+    {
+        templeName: "Bern Switzerland",
+        location: "Bern, Switzerland",
+        dedicated: "1955, September, 11",
+        area: 35546,
+        imageUrl: "images/bern-switzerland-temple.jpg"
+    },
+    {
+        templeName: "Copenhagen Denmark",
+        location: "Copenhagen, Denmark",
+        dedicated: "2004, May, 23",
+        area: 25000,
+        imageUrl: "images/copenhagen-temple.webp"
+    },
+    {
+        templeName: "Lubbock Texas",
+        location: "Lubbock, Texas",
+        dedicated: "2002, April, 21",
+        area: 16498,
+        imageUrl: "images/lubbock-temple.jpg"
+    },
+    {
+        templeName: "Jordan River Utah",
+        location: "South Jordan, Utah",
+        dedicated: "1981, November, 16",
+        area: 148236,
+        imageUrl: "images/jordan-river-temple.jpg"
+    },
     {
         templeName: "Seattle Washington",
         location: "Bellevue, Washington",
-        dedicated: "1980, November, 17-21",
+        dedicated: "1980, November, 17",
         area: 110000,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/seattle-washington-temple/seattle-washington-temple-55800-main.jpg"
+        imageUrl: "images/seattle-temple.jpg"
     },
     {
         templeName: "Snowflake Arizona",
         location: "Snowflake, Arizona",
         dedicated: "2002, March, 2",
         area: 18621,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/snowflake-arizona-temple/snowflake-arizona-temple-46560-main.jpg"
+        imageUrl: "images/snowflake-temple.webp"
     },
-    // removing to try to improve best practices to 95%
-    // { 
-    // templeName: "St. George Utah", 
-    // location: "St. George, Utah",
-    // dedicated: "1877, April, 6-8",
-    // area: 143969,
-    // imageUrl:
-    // "https://churchofjesuschristtemples.org/assets/img/temples/st.-george-utah-temple/st.-george-utah-temple-40435-main.jpg"
-    // },
+
+    {
+        templeName: "St. George Utah",
+        location: "St. George, Utah",
+        dedicated: "1877, April, 6",
+        area: 143969,
+        imageUrl: "images/st-george-utah-temple.jpg"
+    },
     {
         templeName: "Nauvoo Illinois",
         location: "Nauvoo, Illinois",
-        dedicated: "2002, June, 27-30",
+        dedicated: "2002, June, 27",
         area: 54000,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/nauvoo-illinois-temple/nauvoo-illinois-temple-50576-main.jpg"
+        imageUrl: "images/nauvoo-temple.jpg"
     },
     {
         templeName: "Colonia Juarez Chihuahua Mexico",
         location: "Colonia Juarez, Chihuahua, Mexico",
-        dedicated: "1999, March, 6-7",
+        dedicated: "1999, March, 6",
         area: 6800,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/colonia-juarez-chihuahua-mexico-temple/colonia-juarez-chihuahua-mexico-temple-1601-main.jpg"
+        imageUrl: "images/colonia-juarez-mexico-temple.jpg"
+    },
+    {
+        templeName: "Salt Lake City Utah",
+        location: "Salt Lake City, Utah",
+        dedicated: "1893, April, 6",
+        area: 382207,
+        imageUrl: "images/salt-lake-temple.webp"
+    },
+    {
+        templeName: "Albuquerque New Mexico",
+        location: "Albuquerque, New Mexico",
+        dedicated: "2000, March, 5",
+        area: 34245,
+        imageUrl: "images/albuquerque-temple.webp"
+    },
+    {
+        templeName: "San Diego California",
+        location: "San Diego, California",
+        dedicated: "1993, April, 25",
+        area: 72000,
+        imageUrl: "images/san-diego-temple.webp"
     },
 ];
 
